1607687766e50eb0b87bf11e0d9388e2d3a753c3
Mark <mitelea.marcus@proton.me>
2023-11-08 12:40:30 +0100

    WIP user_service init

--- user_service/src/main/java/via/group1/user_service/model/entity/User.java
+++ user_service/src/main/java/via/group1/user_service/model/entity/User.java
@@ -3,8 +3,12 @@ package via.group1.user_service.model.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class User {
 
     @Id
