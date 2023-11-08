c119cfbab5b58e81c034e2d9419feeff9de07b27
Jan Metela <113198396+RubberDuck2307@users.noreply.github.com>
2023-11-08 09:42:27 +0100

    packet_service initial

---
+++ packet_service/src/main/java/via/group1/packet_service/PacketServiceApplication.java
@@ -0,0 +1,13 @@
+package via.group1.packet_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacketServiceApplication.class, args);
+	}
+
+}
---
+++ packet_service/src/test/java/via/group1/packet_service/PacketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package via.group1.packet_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
