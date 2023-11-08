48c09af379fb4439eb295c66c71c87aab5593b6b
AnnaAndrlova <andrlovaanicka@seznam.cz>
2023-11-08 12:40:15 +0100

    setup done

---
+++ frontend/Client/Implementations/PackageHttpClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Client.Interfaces;
+using System.Text.Json;
+
+
+namespace Client.Implementations
+{
+    public class PackageHttpClient: IPackageService{
+        
+            private readonly HttpClient client;
+
+            public PackageHttpClient(HttpClient client)
+            {
+                this.client = client;
+            }
+        
+
+            public async Task<Package> GetPackageByIdAsync(long id)
+            {
+                HttpResponseMessage response = await client.GetAsync($"/package/{id}");
+                string content = await response.Content.ReadAsStringAsync();
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception(content);
+                }
+                Console.WriteLine(content);
+                //use var (packageDTO)
+                var packageDTO = JsonSerializer.Deserialize<PackageGetDTO>(content, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                    IncludeFields = true
+            
+                });
+        
+                Customer sender = new Customer(packageDTO.SenderName);
+                Address receiverAddress = new Address(packageDTO.ReceiverAddress.Street, packageDTO.ReceiverAddress.Number,
+                    packageDTO.ReceiverAddress.City);
+                Address lastKnowLocation = new Address("Kabelikova", 3, "Prerov");
+
+                Package package = new Package(packageDTO.Weight, sender, lastKnowLocation, receiverAddress);
+                return package;
+            }
+    }
+}
---
+++ frontend/Domain/DTO/PackageGetDTO.cs
@@ -0,0 +1,22 @@
+﻿
+public class PackageGetDTO
+{
+    public long SenderId { get; set; }
+    public double Weight { get; set; }
+    public long Id { get; set; }
+    public string Status { get; set; }
+    public string SenderName { get; set; }
+    public string ReceiverName { get; set; }
+    public AddressGetDTO ReceiverAddress { get; set; }
+
+    public PackageGetDTO(long senderId, double weight, long id, string status, string senderName, string receiverName, AddressGetDTO receiverAddress)
+    {
+        SenderId = senderId;
+        Weight = weight;
+        Id = id;
+        Status = status;
+        SenderName = senderName;
+        ReceiverName = receiverName;
+        ReceiverAddress = receiverAddress;
+    }
+}
