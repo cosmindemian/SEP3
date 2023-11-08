a738e4eac450581b3dcc104ab6464ac4ef3c3ca8
Jan Metela <113198396+RubberDuck2307@users.noreply.github.com>
2023-11-08 09:55:57 +0100

    protofiles initial

---
+++ protofiles/src/main/java/via/group1/protofiles/ProtofilesApplication.java
@@ -0,0 +1,13 @@
+package via.group1.protofiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtofilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtofilesApplication.class, args);
+	}
+
+}
