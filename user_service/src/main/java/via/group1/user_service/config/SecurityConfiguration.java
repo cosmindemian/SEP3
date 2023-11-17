b57ea3444d7e5697085c351f914eb0176a6ff9ba
Mark <mitelea.marcus@proton.me>
2023-11-17 09:51:30 +0100

    trying security for user_service

---
+++ user_service/src/main/java/via/group1/user_service/config/SecurityConfiguration.java
@@ -0,0 +1,4 @@
+package via.group1.user_service.config;
+
+public class SecurityConfiguration {
+}
---
+++ user_service/src/main/java/via/group1/user_service/model/interfaces/UserService.java
@@ -0,0 +1,8 @@
+package via.group1.user_service.model.interfaces;
+
+import via.group1.user_service.persistance.entity.User;
+
+public interface UserService {
+    User saveUser(User user);
+
+}
