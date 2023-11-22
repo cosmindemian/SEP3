69bec5699e27009ef25a79c54ce98f345b66ee95
AnnaAndrlova <andrlovaanicka@seznam.cz>
2023-11-22 12:05:01 +0100

    controller for packages in progress

--- gateway/gateway/Controllers/PackageController.cs
+++ gateway/gateway/Controllers/PackageController.cs
@@ -1,11 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using gateway.DTO;
+using gateway.Model;
+using Microsoft.AspNetCore.Mvc;
 
 namespace gateway.Controllers;
-
 
 [ApiController]
 [Route("[controller]")]
 public class PackageController : ControllerBase
 {
-    
+    private readonly IPackage packageLogic;
+
+    public PackageController(IPackage packageLogic)
+    {
+        this.packageLogic = packageLogic;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<GetPackageDto>> GetById([FromRoute] string id)
+    {
+        try
+        {
+            //TODO create a separate class for generating DTOs
+            var package = await packageLogic.GetPackageByTrackingNumber(id);
+            var currentAddress = new GetAddressDto(package.CurrentLocation.Address.Street,
+                package.CurrentLocation.Address.City, package.CurrentLocation.Address.BuildingNumber);
+            var currentLocation = new GetLocationDto(currentAddress, package.CurrentLocation.IsPickupPoint);
+            var finalAddress = new GetAddressDto(package.FinalDestination.Address.Street,
+                package.FinalDestination.Address.City, package.FinalDestination.Address.BuildingNumber);
+            var finalLocation = new GetLocationDto(finalAddress, package.FinalDestination.IsPickupPoint);
+            var dto = new GetPackageDto(package.Id, package.PackageNumber, package.SenderName, package.PackageStatus,
+                package.PackageType, currentLocation, finalLocation);
+            return Ok(dto);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
 }
