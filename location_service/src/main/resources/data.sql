d37c16dc707efe1541b8a3dcfd5d1227286bc051
Cosmin Demian <cosmindemian49@gmail.com>
2023-11-27 13:55:41 +0100

    Dummy data

---
+++ location_service/src/main/resources/data.sql
@@ -0,0 +1,16 @@
+create schema if not exists "location_service";
+
+set schema 'location_service';
+
+INSERT INTO address (id, city, street, street_number, zip)
+VALUES (1, 'Aarhus', 'Hasselager Alle', '22', '8200');
+
+INSERT INTO address (id, city, street, street_number, zip)
+VALUES (2, 'Aarhus', 'Hasselager Alle', '25', '8200');
+
+
+INSERT INTO location (id, address_id, dtype, name, opening_hours, closing_hours)
+VALUES (1, 1, 'PickupPoint', 'Aarhus PickUpPoint', '08:00', '16:00');
+
+INSERT INTO location (id, address_id, dtype)
+VALUES (2, 2, 'Warehouse');
