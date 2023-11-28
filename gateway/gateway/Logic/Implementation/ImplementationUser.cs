71cfedf45a026ee8ee718c5c78f503da881f2bdd
Mark <mitelea.marcus@proton.me>
2023-11-28 13:01:02 +0100

    Update ImplementationUser.cs

--- gateway/gateway/Logic/Implementation/ImplementationUser.cs
+++ gateway/gateway/Logic/Implementation/ImplementationUser.cs
@@ -1,10 +1,12 @@
-﻿using RpcClient.RpcClient.Interface;
+﻿using gateway.DTO;
+using RpcClient.RpcClient.Interface;
 
 namespace gateway.Model.Implementation;
 
 public class ImplementationUser
 {
     private readonly IUserServiceClient _userServiceClient;
+    private readonly DtoMapper _dtoMapper;
 
 
     public ImplementationUser(IUserServiceClient userServiceClient)
