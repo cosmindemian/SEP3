1dfef6a073750521566dda72548fe8ed1f452f79
Reblayzer <104923443+Reblayzer@users.noreply.github.com>
2023-11-28 14:24:29 +0100

    fixing dummy data

--- packet_service/src/main/resources/data.sql
+++ packet_service/src/main/resources/data.sql
@@ -9,9 +9,9 @@ INSERT INTO packet_service.packet(id, tracking_number, current_location_id, fina
                                   receiver_id, status_id)
 VALUES (1, 'P1', 1, 2, 1, 2, 1);
 
-insert into packet_service.size(id, name, length, width, height)
-values (1, 'name1', 'length1', 'width1', 'height1');
-insert into packet_service.size(id, name, length, width, height)
-values (2, 'name2', 'length2', 'width2', 'height2');
-insert into packet_service.size(id, name, length, width, height)
-values (3, 'name3', 'length3', 'width3', 'height3');
+insert into packet_service.size(id, name, length, width, height,weight)
+values (1, 'name1', 'length1', 'width1', 'height1','weight1');
+insert into packet_service.size(id, name, length, width, height,weight)
+values (2, 'name2', 'length2', 'width2', 'height2','weight2');
+insert into packet_service.size(id, name, length, width, height,weight)
+values (3, 'name3', 'length3', 'width3', 'height3','weight3');
