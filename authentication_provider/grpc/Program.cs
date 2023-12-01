f1266bd60728dc9e727bfafdf569d9db3b4c5326
Mark <mitelea.marcus@proton.me>
2023-12-01 10:21:47 +0100

    grpc impl for email code

--- authentication_provider/grpc/Program.cs
+++ authentication_provider/grpc/Program.cs
@@ -1,9 +1,9 @@
 using System.Text;
+using grpc.Config;
 using grpc.Logic;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using persistance;
-using persistance.Config;
 using persistance.Dao;
 
 var builder = WebApplication.CreateBuilder(args);
